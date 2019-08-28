41b6dc8d50051ca8a34fb8ab6a22afade2c4de4d
Mykhailo <molochko@adobe.com>
2019-08-28 12:55:06 +0100

    encoder bash script

---
+++ encoder.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+find Path/To/Xml/Files -name *.xml -type f | \
+(while read file;
+    do if [[ "$file" != *.DS_Store* ]]; then
+    if [[ "$file" != *-utf8* ]]; then
+        iconv -f ISO-8859-1 -t UTF-8 "$file" > "$file-utf8";
+        rm $file;
+        echo mv "$file-utf8" "$file";
+        mv "$file-utf8" "$file";
+    fi
+fi
+done);
